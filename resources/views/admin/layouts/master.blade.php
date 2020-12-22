0a172d033ebf86bb67a1f94c1389ca087aad02f2
ujangaris <ujangaja@gmail.com>
2020-12-22 07:29:03 +0700

    integrasi Template Admin LTE

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html>
+@include('admin.layouts.partials.head')
+<body class="hold-transition skin-blue sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+
+  @include('admin.layouts.partials.header')
+
+  <!-- =============================================== -->
+
+  <!-- Left side column. contains the sidebar -->
+  @include('admin.layouts.partials.sidebar')
+
+  <!-- =============================================== -->
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Blank page
+        <small>it all starts here</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      @yield('content')
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  @include('admin.layouts.partials.footer')
+
+  <!-- Control Sidebar -->
+  @include('admin.layouts.partials.sidebar-control')
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+@include('admin.layouts.partials.scripts')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::prefix('admin')->group(function(){
+    Route::get('dashboard', 'HomeController@index');
+});
