d8479f93da60687c7113a166d4b58c48759fcb84
ujangaris <ujangaja@gmail.com>
2021-01-01 08:48:28 +0700

    Menghilangkan admin dari view sebagai protection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = User::all();
+        $user = User::where('id', '!=', Auth::user()->id)->get();
         return view('admin.user.index', compact('user'));
     }
 }
