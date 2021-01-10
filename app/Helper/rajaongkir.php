b7df3ed43878586ffae4117510a3c6f85b671262
ujangaris <ujangaja@gmail.com>
2021-01-11 05:30:50 +0700

    Integrasi rajaongkir | menampilkan json data city

--- app/Helper/rajaongkir.php
+++ app/Helper/rajaongkir.php
@@ -1,7 +1,31 @@
 <?php
 if( !function_exists('city')){
 	function city(){
-        echo "Haii";
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.rajaongkir.com/starter/city",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "key: b27b9bb1e538fcef9fcf3ea48b231d81"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
     }
 
 }
