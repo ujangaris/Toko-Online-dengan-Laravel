3a1db82a1d1098eba9855c957ec63840c0455412
ujangaris <ujangaja@gmail.com>
2021-01-20 16:02:55 +0700

    Memasang discuse comentar

--- resources/views/homepage/detail.blade.php
+++ resources/views/homepage/detail.blade.php
@@ -87,74 +87,26 @@
                   <li class="list-inline-item"><a href="#" data-animate-hover="pulse" class="email"><i class="fa fa-envelope"></i></a></li>
                 </ul>
               </div>
-              <div class="row">
-                <div class="col-lg-3 col-md-6">
-                  <div class="box text-uppercase mt-0 mb-small">
-                    <h3>You may also like these products</h3>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                  <div class="product">
-                    <div class="image"><a href="#"><img src="img/product2.jpg" alt="" class="img-fluid image1"></a></div>
-                    <div class="text">
-                      <h3 class="h5"><a href="shop-detail.html">Fur coat</a></h3>
-                      <p class="price">$143</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                  <div class="product">
-                    <div class="image"><a href="#"><img src="img/product3.jpg" alt="" class="img-fluid image1"></a></div>
-                    <div class="text">
-                      <h3 class="h5"><a href="shop-detail.html">Fur coat</a></h3>
-                      <p class="price">$143</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                  <div class="product">
-                    <div class="image"><a href="#"><img src="img/product1.jpg" alt="" class="img-fluid image1"></a></div>
-                    <div class="text">
-                      <h3 class="h5"><a href="shop-detail.html">Fur coat</a></h3>
-                      <p class="price">$143</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-lg-3 col-md-6">
-                  <div class="box text-uppercase mt-0 mb-small">
-                    <h3>Products viewed recently</h3>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                  <div class="product">
-                    <div class="image"><a href="#"><img src="img/product3.jpg" alt="" class="img-fluid image1"></a></div>
-                    <div class="text">
-                      <h3 class="h5"><a href="shop-detail.html">Fur coat</a></h3>
-                      <p class="price">$143</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                  <div class="product">
-                    <div class="image"><a href="#"><img src="img/product1.jpg" alt="" class="img-fluid image1"></a></div>
-                    <div class="text">
-                      <h3 class="h5"><a href="shop-detail.html">Fur coat</a></h3>
-                      <p class="price">$143</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                  <div class="product">
-                    <div class="image"><a href="#"><img src="img/product2.jpg" alt="" class="img-fluid image1"></a></div>
-                    <div class="text">
-                      <h3 class="h5"><a href="shop-detail.html">Fur coat</a></h3>
-                      <p class="price">$143</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <div id="disqus_thread"></div>
+                <script>
+                    /**
+                    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+
+                    var disqus_config = function () {
+                    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier = {{ $products->id }}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                    };
+
+                    (function() { // DON'T EDIT BELOW THIS LINE
+                    var d = document, s = d.createElement('script');
+                    s.src = 'https://ujangarisandi-com.disqus.com/embed.js';
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
             </div>
             <div class="col-md-3">
                 <!-- MENUS AND FILTERS-->
